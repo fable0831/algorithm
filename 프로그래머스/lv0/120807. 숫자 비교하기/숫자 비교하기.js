0325d4822e6dc9638636d29dc6ea2e932a513fb0
JoosangKim <fable0831@gmail.com>
2023-03-02 09:15:31 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,9 +1,3 @@
 function solution(num1, num2) {
-    let answer = null;
-    if(num1 === num2){
-        answer = 1;
-    } else {
-        answer=-1;
-    }
-    return answer;
+   return num1 === num2 ? 1 : -1
 }
