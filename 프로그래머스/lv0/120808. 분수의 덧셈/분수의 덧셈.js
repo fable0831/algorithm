d529b5775b974de3cb24c8981ce0a44859ebf13c
JoosangKim <fable0831@gmail.com>
2023-03-02 09:59:17 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,10 +1,13 @@
 function solution(numer1, denom1, numer2, denom2) {
-    let answer = []
-    let denom = denom1*denom2
-    let numer = numer1*denom2 + numer2*denom1
-    for (denom > numer? i = numer : i = denom; i>0; i--){
-        if(denom % i === 0 && numer%i === 0) {
-            return answer = [numer/i, denom/i];
-        }
-    }
+    let denom = denom1*denom2;
+    let numer = numer1*denom2 + numer2*denom1;
+    let minNum = denom > numer ? numer : denom;
+    
+    while (true){
+        if(denom % minNum === 0 && numer%minNum === 0) {
+            return [numer/minNum, denom/minNum]
+        } else {
+            minNum = minNum -1
+        };
+    };
 }
