afcc934d03ee1f72436376d85dc3e02813f3c4ef
JoosangKim <fable0831@gmail.com>
2023-03-02 11:03:47 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,13 +1,5 @@
 function solution(array) {
-    array.sort(function(a,b){
-        if(a>b){
-            return 1;
-        } else if (a ===b) {
-            return 0;
-        } else if(a<b) {
-            return -1;
-        }
-    }) ;
-    let i = Math.floor(array.length/2);
-    return array[i]
+    return array.sort((a,b) => (
+        a-b
+    ))[Math.floor(array.length/2)];
 }
