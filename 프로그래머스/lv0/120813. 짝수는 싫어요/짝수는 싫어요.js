c5210202ed38405af4f3ac9bd2b504b5448d7bde
JoosangKim <fable0831@gmail.com>
2023-03-02 12:05:14 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,12 +1,9 @@
 function solution(n) {
     var answer = [];
-    let odd = n%2 === 0 ? n-1 : n 
-    for (i = odd; i>0;){
-        answer.push(i);
-        i = i-2;
-    }
     
-    answer.sort((a,b) => a-b);
+    for (i = 1; i<=n; i+=2){
+        answer.push(i);
+    }
     
     return answer;
 }
