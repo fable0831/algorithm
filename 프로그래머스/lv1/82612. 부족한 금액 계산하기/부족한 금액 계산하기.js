8c963fb46eab7e4aef437af9173239f5677a46a4
JoosangKim <fable0831@gmail.com>
2023-06-02 10:31:10 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,8 +1,8 @@
 function solution(price, money, count) {
     var answer = 0;
     for(let i = 1; i<= count; i++){
-        answer += i;
+        answer += price * i;
     }
     
-    return money >= answer * price ? 0 : answer * price - money
+    return money >= answer ? 0 : answer - money
 }
