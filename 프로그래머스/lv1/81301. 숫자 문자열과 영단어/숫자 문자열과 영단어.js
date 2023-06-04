3de6aa28b1f340101063a7802ee8dbe8844dca70
JoosangKim <fable0831@gmail.com>
2023-06-04 21:58:47 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,9 +1,12 @@
 function solution(s) {
-    var answer = 0;
+    var answer = s;
     let text = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let num = [0,1,2,3,4,5,6,7,8,9];
     for (let i = 0; i<10; i++){
-        s = s.split(text[i]).join(num[i]);
+        answer = answer.split(text[i]).join(num[i]);
     }
-    return answer = parseInt(s);
+    return parseInt(answer);
 }
+
+//split으로 구분자를 공백으로 대체하면서 배열 생성
+//공백으로 대체된 구분자를 num[i]를 넣고 문자열로 변한
