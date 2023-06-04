4624065c1c25e5a4bee966c1f6b09ec6495e4665
JoosangKim <fable0831@gmail.com>
2023-06-04 21:54:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    var answer = 0;
+    let text = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let num = [0,1,2,3,4,5,6,7,8,9];
+    for (let i = 0; i<10; i++){
+        s = s.split(text[i]).join(num[i]);
+    }
+    return answer = parseInt(s);
+}
