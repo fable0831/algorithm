1033c4e9f9e6802054e682c1cceb7a60978babc1
JoosangKim <fable0831@gmail.com>
2023-06-07 10:51:26 +0900

    [unrated] Title: 과일 장수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,11 @@
+function solution(k, m, score) {
+    var answer = 0;
+    score = score.sort((a,b) => b-a);
+    let max = parseInt(score.length/m);
+    
+    for(let i = 1; i<=max; i++){
+        answer += score[m*i-1] * m;
+    }
+    
+    return answer;
+}
